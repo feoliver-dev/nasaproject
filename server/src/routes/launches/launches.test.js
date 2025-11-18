359ab83e07a149371ae4059d25a4cee67d1dff43
FernandaOliveira <foliveira@jazida.com>
2025-11-18 15:54:22 -0300

    incluindo supertest e atualizando organização dos modulos

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+
+const app = require('../../app');
+
+describe('Test GET /launches', () => {
+    test('It should respond with 200 success', async () => {
+        const response = await request(app).get('/launches');
+        expect(response.statusCode).toBe(200);
+    });
+});
+
+describe('Test POST /launch', () => {
+    test('It should respond with 200 created', async () => {
+
+    });
+
+    test('It should catch missing required properties', async () => {
+
+    });
+    
+    test('It should catch invalid dates', async () => {});
+});
