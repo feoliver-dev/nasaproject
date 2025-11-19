0c9628257f75518c39132aabe47aa34944d10593
FernandaOliveira <foliveira@jazida.com>
2025-11-19 15:52:14 -0300

    acrescentando arquivo de versão do nvm

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -4,14 +4,31 @@ const app = require('../../app');
 
 describe('Test GET /launches', () => {
     test('It should respond with 200 success', async () => {
-        const response = await request(app).get('/launches');
-        expect(response.statusCode).toBe(200);
+        const response = await request(app)
+        .get('/launches')
+        .expect('Content-Type', /json/)
+        .expect(200);
     });
 });
 
 describe('Test POST /launch', () => {
     test('It should respond with 200 created', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send({
+            mission: 'USS Enterprise',
+            rocket: 'NCC 1701-D',
+            target: 'Kepler-186 f',
+            launchDate: 'January 4, 2028',
+        })
+        .expect('Content-Type', /json/)
+        .expect(201);
 
+        const requestDate = new Date('January 4, 2028').valueOf();
+        const responseDate = new Date(response.body.launchDate).valueOf();
+        expect(responseDate).toBe(requestDate);
+
+        expect(response.body).toMatchObject({launchDataWithoutDate: {}});
     });
 
     test('It should catch missing required properties', async () => {
